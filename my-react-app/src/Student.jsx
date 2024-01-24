531c383162c483607a9a476dd9dd1886f1dce351
Minura Dinupriya <mranaweera793@gmail.com>
2024-01-24 09:22:10 +0530

    props

---
+++ my-react-app/src/Student.jsx
@@ -0,0 +1,9 @@
+export function Student(props){
+    return(
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent?"yes":"no"}</p>
+        </div>
+    );
+}
