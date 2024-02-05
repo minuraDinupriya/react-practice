a1eaf02a4c8cbb398cd170bac292b26df7b87cce
Minura Dinupriya <mranaweera793@gmail.com>
2024-02-05 16:39:22 +0530

    tic-tac-toe X gets rerendered when its button is clicked

--- my-react-app/src/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export function Button(){
-    const handleClicks=(e)=>console.log(e);
-    return(
-        <button onClick={(e)=>handleClicks(e)}>Click me</button>
-    );
-}
--- my-react-app/src/app.jsx
+++ my-react-app/src/app.jsx
@@ -1,25 +1,35 @@
 import { useState } from 'react';
+function Square(){
+    const [value, setValue  ] = useState(null)
+    function handleClick(){
+        setValue("X");
+        console.log("You Clicked")
+    }
 
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-
-  function clickFunc() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={clickFunc} />
-      <MyButton count={count} onClick={clickFunc} />
-    </div>
-  );
+    return(
+        <button className="square" onClick={handleClick}>{value}</button>
+    )
 }
 
-function MyButton({ count, func }) {
-  return (
-    <button onClick={func}>
-      Clicked {count} times
-    </button>
-  );
+export default function Board() {
+     return(
+        <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+     );
 }
+
