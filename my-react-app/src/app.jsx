6b4fc4edc8d57cedbfef4e92197eeab1084abbc7
Minura Dinupriya <mranaweera793@gmail.com>
2024-02-05 13:29:54 +0530

    passing properties of parents to their childrens

--- my-react-app/src/app.jsx
+++ my-react-app/src/app.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
-
-function MyButton() {
+export default function MyApp() {
   const [count, setCount] = useState(0);
 
-  function handleClick() {
+  function clickFunc() {
     setCount(count + 1);
   }
 
   return (
-    <button onClick={handleClick}>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={clickFunc} />
+      <MyButton count={count} onClick={clickFunc} />
+    </div>
+  );
+}
+
+function MyButton({ count, func }) {
+  return (
+    <button onClick={func}>
       Clicked {count} times
     </button>
   );
